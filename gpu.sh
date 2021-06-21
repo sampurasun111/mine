43f363cc79863790eb37a83437bb18fc97d9af43
sampurasun111 <86062427+sampurasun111@users.noreply.github.com>
2021-06-21 14:40:19 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-POOL=asia-etc.2miners.com:1010
+POOL=etc.2miners.com:1010
 WALLET=0x24766e8c6d39968a9615b8e7270c498b97f98152
-WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER 
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-GPUNYOLONG)
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
